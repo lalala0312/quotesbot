10d8ea5635bb70fe875f967fe20384e07aca9eeb
lalala0312 <1503647333@qq.com>
2023-02-22 18:33:03 +0800

    Imported Item and Field in item.py and defined text, author, tags in QuotesbotItem class

--- quotesbot/items.py
+++ quotesbot/items.py
@@ -5,10 +5,10 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
 
-class QuotesbotItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class QuotesbotItem(Item):
+    text = Field()
+    author = Field()
+    tags = Field()
